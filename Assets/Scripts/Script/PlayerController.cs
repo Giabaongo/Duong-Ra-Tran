1ca4c6537397e0be481cfc2fa93203b2cf927dd0
Giabaongo <giabaongo1@gmail.com>
2025-10-19 14:10:38 +0700

    [UPDATE] add Rotate towards mouse

--- Assets/Scripts/Script/PlayerController.cs
+++ Assets/Scripts/Script/PlayerController.cs
@@ -44,6 +44,30 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("IsAttacking", false);
         }
 
+        // Xoay nhân v?t theo h??ng con tr? chu?t
+        RotateTowardsMouse();
+    }
+
+    void RotateTowardsMouse()
+    {
+        // L?y v? trí con tr? chu?t trong th? gi?i game
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f;
+
+        // Tính h??ng t? nhân v?t ??n con tr? chu?t
+        Vector2 direction = (mousePosition - transform.position).normalized;
+
+        // Flip sprite theo h??ng trái/ph?i
+        if (direction.x > 0)
+        {
+            // Quay ph?i
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        else if (direction.x < 0)
+        {
+            // Quay trái
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
     }
 
  
