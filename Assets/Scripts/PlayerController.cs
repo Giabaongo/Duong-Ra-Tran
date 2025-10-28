1344017acd30102ac67141a2cce2800f1f5c459c
Giabaongo <giabaongo1@gmail.com>
2025-10-28 13:23:54 +0700

    [MERGE] branch pung to dev

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 1f;
 
--- Assets/Scripts/Script/PlayerShooting.cs
+++ Assets/Scripts/Script/PlayerShooting.cs
@@ -6,32 +6,52 @@ public class PlayerShooting : MonoBehaviour
     public Transform bulletPos;
     private GameObject player;
     private float timer;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        
+        if (player == null)
+        {
+            Debug.LogError("Player not found! Make sure Player has 'Player' tag.");
+        }
+        
+        if (bullet == null)
+        {
+            Debug.LogWarning("Bullet prefab not assigned in " + gameObject.name);
+        }
+        
+        if (bulletPos == null)
+        {
+            Debug.LogWarning("BulletPos not assigned in " + gameObject.name);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (player == null) return;
+        
         float distance = Vector2.Distance(transform.position, player.transform.position);
-        Debug.Log(distance);
+        
         if (distance < 4)
         {
             timer += Time.deltaTime;
 
             if (timer > 2)
             {
-
                 timer = 0;
                 Shoot();
             }
         }
-                    }
+    }
 
     void Shoot()
     {
-        Instantiate(bullet, bulletPos.position, Quaternion.identity);
+        if (bullet != null && bulletPos != null)
+        {
+            Instantiate(bullet, bulletPos.position, Quaternion.identity);
+        }
     }
 }
